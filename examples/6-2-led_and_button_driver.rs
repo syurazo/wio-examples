ab6d1fea97fb2ca064dc8f5d3e087e8f862c9cdc
syurazo <syurazo@gmail.com>
2021-04-29 12:45:12 +0900

    button1 の制御を Button1 ドライバに分離

--- examples/6-2-led_and_button_driver.rs
+++ examples/6-2-led_and_button_driver.rs
@@ -25,10 +25,10 @@ fn main() -> ! {
     let mut pins = wio::Pins::new(peripherals.PORT);
 
     let mut led = pins.user_led.into_push_pull_output(&mut pins.port);
-    let button1 = pins.button1.into_floating_input(&mut pins.port);
+    let button1 = Button1::new(pins.button1, &mut pins.port);
 
     loop {
-        if button1.is_low().unwrap() {
+        if button1.is_pressed() {
             led.set_high().unwrap();
         } else {
             led.set_low().unwrap();
@@ -36,8 +36,25 @@ fn main() -> ! {
     }
 }
 
-// Wio Terminalのボタン1ドライバ
-// TODO: Button1 を実装する
+struct Button1 {
+    pin: Pc26<Input<Floating>>,
+}
+
+impl Button1 {
+    fn new(pin: Pc26<Input<Floating>>, port: &mut Port) -> Button1 {
+        Button1 {
+            pin: pin.into_floating_input(port),
+        }
+    }
+
+    fn is_pressed(&self) -> bool {
+        self.pin.is_low().unwrap()
+    }
+
+    fn is_released(&self) -> bool {
+        self.pin.is_high().unwrap()
+    }
+}
 
 // Wio TerminalのユーザーLEDドライバ
 // TODO: Led を実装する
